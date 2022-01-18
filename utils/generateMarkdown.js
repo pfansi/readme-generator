569b98cba31c2706edb8cd53a2edb14d5f96a0f0
pfansiW <fancyfreelance76@gmail.com>
2022-01-18 03:58:38 +0000

    created function that will render the licence link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,7 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// this function will return the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
